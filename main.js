9583caecf9f79882eb0aac1dfdb6e6a6cc408bbb
Maftuna <156097566+maftunarozimatova@users.noreply.github.com>
2024-02-23 19:19:27 +0500

    variables

---
+++ main.js
@@ -0,0 +1,41 @@
+
+// Qo'shish
+
+let Jeki = 56
+let Jenni = 18
+console.log(Jeki + Jenni);
+
+// Ayirish
+
+let Tom = 73
+let Roma = 38
+let all = Tom - Roma
+console.log(all)
+
+// Ko'payrtirish
+
+let products = 42
+let price = 15
+let total = products * price
+console.log(total)
+
+// Qo'shish va Ayirish
+
+let  needSpend = 780
+let  praduct = 170
+let  clothingStore = 390
+let increased = needSpend - praduct - clothingStore
+console.log(increased);
+let ishlatildi = praduct + clothingStore
+console.log(ishlatildi);
+
+// Bo'lish
+
+let mahsulot = 320
+let narxi = 72
+let Total = mahsulot * narxi
+let magazine1 = (Total / 100 ) * 52
+let magazine2 = (Total / 100) * 48
+console.log("Total " + Total);
+console.log("magazine1 " + magazine1);
+console.log("magazine2 " + magazine2);
